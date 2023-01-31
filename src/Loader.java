ae00a43dd5a2532b72f7e50d33b0904fc402ff33
olgaa <Tomashova_ON@mail.ru>
2023-01-31 16:34:17 +0800

    сообщение

---
+++ src/Loader.java
@@ -0,0 +1,12 @@
+public class Loader {
+    public static void main(String[] args) {
+        int i = 10;
+        do {i--;}
+
+         while ( i > 1);
+
+        //Код писать здесь
+
+        System.out.println("Value i = " + i);
+    }
+}
